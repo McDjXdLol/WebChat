1a166ca87164f4a5f22fa16c038d430b85af5cea
McDjXdLol <mcdjxdlol@gmail.com>
2025-08-07 08:53:21 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_socketio import SocketIO, emit, disconnect
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+connected_users = {}  # {nickname: sid}
+sid_to_nickname = {}  # {sid: nickname}
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username', 'Guest')
+    color = request.args.get('color', '#ffffff')
+
+    if username in connected_users:
+        return redirect(url_for('login', error='nickname'))
+
+    first_letter_username = username[0]
+    return render_template('chat.html', username=username, color=color, flu=first_letter_username)
+
+
+@app.route('/login')
+def login():
+    error = request.args.get('error')
+    return render_template('login.html', error=error)
+
+
+@socketio.on('connect')
+def handle_connect():
+    print("New connection:", request.sid)
+
+
+@socketio.on('register_user')
+def handle_register_user(data):
+    username = data.get("username")
+    if username in connected_users:
+        emit('nickname_taken')
+        disconnect()
+        return
+
+    connected_users[username] = request.sid
+    sid_to_nickname[request.sid] = username
+    print(f"Registered: {username} ({request.sid})")
+    emit('registration_successful')
+
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    sid = request.sid
+    nickname = sid_to_nickname.pop(sid, None)
+    if nickname:
+        connected_users.pop(nickname, None)
+        print(f"{nickname} disconnected - ({sid})")
+    else:
+        print(f"Unknown client disconected - ({sid})")
+
+
+@socketio.on('send_message')
+def handle_message(data):
+    print('Received:', data)
+    emit('receive_message', data, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
