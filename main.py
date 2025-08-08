405fa158b05beaaa162a3e1daea1ea87123ad9e1
McDjXdLol <mcdjxdlol@gmail.com>
2025-08-08 09:55:11 +0200

    fix: fixed side bar avatar color
    fixed the problem with: background color of avatar on the side was not the same as color chosen by user. And changed info display type. and minor changes

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 import os
 from datetime import datetime
-
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_socketio import SocketIO, emit
-
 from bot import Bot
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'fallback')
 socketio = SocketIO(app)
 
-online_users = set()
-connected_users = {}  # {nickname: sid}
-sid_to_nickname = {}  # {sid: nickname}
-messages = []  # [{"datetime": "{datetime}","message": "{msg}","username": "{nick}}"}]
-
-unique_users = set()  # Set to track unique users
-unique_users_count = 0  # Counter for unique users
+online_users = {}  # {username: color}
+sid_to_nickname = {}  # {sid: username}
+unique_users = set() # Set to store unique usernames
+unique_users_count = 0 # Counter for unique users
+messages = []  # List to store chat messages
 
 bot = Bot()
 
@@ -25,17 +22,20 @@ bot = Bot()
 def home():
     return render_template("index.html")
 
+
 @app.route('/api')
 def api():
     return render_template("api.html")
+
 
 @app.route('/api/messages_history')
 def messages_history():
     return jsonify(messages)
 
+
 @app.route('/api/unique_users')
 def unique_users_count_api():
-    if unique_users is None or len(unique_users) == 0:
+    if not unique_users:
         return jsonify({"unique_users": [], "amount": 0})
     uniq_users_list = sorted(list(unique_users))
     return jsonify({"unique_users": uniq_users_list, "amount": unique_users_count})
@@ -43,7 +43,7 @@ def unique_users_count_api():
 
 @app.route('/api/users_online')
 def users_online():
-    return jsonify({"online_users": list(online_users), "amount": len(online_users)})
+    return jsonify({"online_users": list(online_users.keys()), "amount": len(online_users)})
 
 
 @app.route('/healthz')
@@ -54,9 +54,9 @@ def healthz():
 @app.route('/chat')
 def chat():
     username = request.args.get('username', 'Guest')
-    color = request.args.get('color', '#ffffff')
+    color = request.args.get('color', None)  # moze być None - wtedy nadamy samemu
 
-    if username in connected_users:
+    if username in online_users:
         return redirect(url_for('login', error='nickname'))
 
     first_letter_username = username[0]
@@ -73,6 +73,7 @@ def login():
 def about():
     return render_template('about.html')
 
+
 @socketio.on('user_typing')
 def handle_typing(data):
     emit('show_typing', data, broadcast=True, include_self=False)
@@ -80,7 +81,8 @@ def handle_typing(data):
 
 @socketio.on('user_count')
 def handle_user_count():
-    emit('update_users_online', {'users': list(online_users)}, broadcast=True)
+    username = sid_to_nickname.get(request.sid)
+    emit('update_users_online', {'users': [{'username': u, 'color': c} for u, c in online_users.items()], "user_color": online_users.get(username)}, broadcast=True)
 
 
 @socketio.on('connect')
@@ -90,22 +92,28 @@ def handle_connect():
 
 @socketio.on('register_user')
 def handle_register(data):
+    global unique_users_count
     username = data.get('username')
+    color = data.get('color')
+
     sid = request.sid
     if username in online_users:
         emit('nickname_taken')
         return
-    if not username in unique_users:
-        global unique_users_count
+    if username not in unique_users:
         unique_users.add(username)
         unique_users_count += 1
         print(f"New Unique user: {username}! Total unique users: {unique_users_count}")
-    online_users.add(username)
+
+    if not color:
+        color = '#' + ''.join(random.choices('0123456789abcdef', k=6))
+
+    online_users[username] = color
     sid_to_nickname[sid] = username
 
     emit('registration_successful')
 
-    emit('update_users_online', {'users': list(online_users)}, broadcast=True)
+    emit('update_users_online', {'users': [{'username': u, 'color': c} for u, c in online_users.items()]}, broadcast=True)
 
 
 @socketio.on('disconnect')
@@ -113,10 +121,9 @@ def handle_disconnect():
     sid = request.sid
     username = sid_to_nickname.get(sid)
     if username:
-        online_users.discard(username)
-        del sid_to_nickname[sid]
-
-        emit('update_users_online', {'users': list(online_users)}, broadcast=True)
+        online_users.pop(username, None)
+        sid_to_nickname.pop(sid, None)
+        emit('update_users_online', {'users': [{'username': u, 'color': c} for u, c in online_users.items()]}, broadcast=True)
 
 
 @socketio.on('send_message')
@@ -125,8 +132,12 @@ def handle_message(data):
 
     emit('receive_message', data, broadcast=True)
 
-    messages.append({"message": data["text"], "username": data['username'],
-                     "datetime": datetime.now().strftime("%d.%m.%Y %H:%M:%S")})
+    messages.append({
+        "message": data["text"],
+        "username": data['username'],
+        "datetime": datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    })
+
     if bot.is_command(data["text"]) or data["text"].startswith('/'):
         response = bot.get_reply(data["text"])
         if response:
@@ -136,8 +147,11 @@ def handle_message(data):
                 'text': response
             }, broadcast=True)
 
-        messages.append({"message": response, "username": "Bot",
-                         "datetime": datetime.now().strftime("%d.%m.%Y %H:%M:%S")})
+            messages.append({
+                "message": response,
+                "username": "Bot",
+                "datetime": datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+            })
 
 
 if __name__ == '__main__':
